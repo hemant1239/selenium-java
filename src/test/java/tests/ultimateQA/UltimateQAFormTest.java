ed3743c85b0a3422a3824dffce184922b703dd54
Hemant <hemantonguitar@yahoo.com>
2025-08-22 21:29:10 +0530

    Updated framework

--- src/test/java/tests/ultimateQA/UltimateQAFormTest.java
+++ src/test/java/tests/ultimateQA/UltimateQAFormTest.java
@@ -1,24 +1,34 @@
 package tests.ultimateQA;
 
 import com.aventstack.extentreports.MediaEntityBuilder;
+import com.fasterxml.jackson.databind.JsonNode;
 import framework.pages.UltimateQA.FormPage;
+import framework.utils.JsonUtils;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class UltimateQAFormTest extends BaseTest {
-
     FormPage formPage;
+    private JsonNode formTestData;
+    private JsonNode messageConstants;
+
+    @BeforeClass
+    public void dataSetup() {
+        formTestData = JsonUtils.getJsonNode("src/test/resources/testdata/ultimateQA/formTestData.json");
+        messageConstants = JsonUtils.getJsonNode("src/test/resources/testdata/ultimateQA/messageConstants.json");
+    }
 
     @Test(priority = 1)
-    public void validateFormSuccessMessage() throws Exception {
+    public void shouldSubmitFormSuccessfully() throws Exception {
         formPage = new FormPage(driver);
         formPage.clickOnFormLink();
         test.info("Clicked on form link");
-        formPage.enterName(FormPage.NAME);
-        formPage.enterMessage(FormPage.MESSAGE);
+        formPage.enterName(formTestData.get("formFieldsTest").get("name").asText());
+        formPage.enterMessage(formTestData.get("formFieldsTest").get("message").asText());
         test.info("Added details in text fields");
         formPage.clickOnSubmit();
-        Assert.assertEquals(formPage.getSuccessMessage(), FormPage.FORM_SUBMIT_SUCCESS);
+        Assert.assertEquals(formPage.getSuccessMessage(), messageConstants.get("formMessages").get("formSuccess").asText(), "Success message is not as expected");
         test.pass(MediaEntityBuilder.createScreenCaptureFromBase64String(scUtil.getExtentScreenShot()).build());
     }
 
@@ -27,11 +37,11 @@ public class UltimateQAFormTest extends BaseTest {
         formPage = new FormPage(driver);
         formPage.clickOnFormLink();
         test.info("Clicked on form link");
-        formPage.enterNameCaptchaForm(FormPage.NAME);
-        formPage.enterMessageCaptchaForm(FormPage.MESSAGE);
+        formPage.enterNameCaptchaForm(formTestData.get("formFieldsTest").get("name").asText());
+        formPage.enterMessageCaptchaForm(formTestData.get("formFieldsTest").get("message").asText());
         test.info("Added details in text fields");
         formPage.clickOnSubmitCaptchaForm();
-        Assert.assertEquals(formPage.getEmptyFieldName(), FormPage.CAPTCHA);
+        Assert.assertEquals(formPage.getEmptyFieldName(), messageConstants.get("formMessages").get("captcha").asText(), "Empty captcha message is not as expected");
         test.pass(MediaEntityBuilder.createScreenCaptureFromBase64String(scUtil.getExtentScreenShot()).build());
     }
 }
--- src/test/java/tests/ultimateQA/UltimateQASignInTest.java
+++ src/test/java/tests/ultimateQA/UltimateQASignInTest.java
@@ -1,23 +1,36 @@
 package tests.ultimateQA;
 
 import com.aventstack.extentreports.MediaEntityBuilder;
+import com.fasterxml.jackson.databind.JsonNode;
 import framework.pages.UltimateQA.SignInPage;
+import framework.utils.ConfigReader;
+import framework.utils.JsonUtils;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class UltimateQASignInTest extends BaseTest {
 
     SignInPage signInPage;
+    private JsonNode messageConstants;
+
+    @BeforeClass
+    public void dataSetup() {
+        messageConstants = JsonUtils.getJsonNode("src/test/resources/testdata/ultimateQA/messageConstants.json");
+    }
 
     @Test
     public void validateInvalidCredentials() throws Exception {
         signInPage = new SignInPage(driver);
         signInPage.clickLoginLink();
-        signInPage.enterEmail(SignInPage.invalidUserName);
-        signInPage.enterPassword(SignInPage.invalidPassword);
+        signInPage.enterEmail(ConfigReader.get("invalidUserName"));
+        logger.info("Entered user name");
+        signInPage.enterPassword(ConfigReader.get("invalidPassword"));
+        logger.info("Entered password");
         test.info("Entered credentials");
         signInPage.clickSignIn();
-        Assert.assertEquals(signInPage.getSignInErrorMessage(), SignInPage.signInErrorMessage);
+        logger.info("Clicked on sign in button");
+        Assert.assertEquals(signInPage.getSignInErrorMessage(), messageConstants.get("signInMessages").get("invalidCredentialsMessage").asText(), "Sign in invalid validation failed");
         test.pass(MediaEntityBuilder.createScreenCaptureFromBase64String(scUtil.getExtentScreenShot()).build());
     }
 }
